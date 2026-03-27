d024e077ff44b1afa72ef791508593ad64e56b6a
Crond-Fx <Crond.Fx@gmail.com>
2026-03-27 03:03:40 +0300

    Update script source path for zoom.js

--- quartz/components/Scripts.tsx
+++ quartz/components/Scripts.tsx
@@ -4,7 +4,7 @@ function Scripts() {
   return (
     <>
       <script src="https://cdn.jsdelivr.net/npm/medium-zoom@1.1.0/dist/medium-zoom.min.js"></script>
-      <script src="/static/js/zoom.js"></script>
+      <script src="../static/js/zoom.js"></script>
     </>
   )
 }
