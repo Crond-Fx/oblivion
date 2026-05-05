982d9f60c07b9459ace3271d13e0337296848fdf
Crond-Fx <Crond.Fx@gmail.com>
2026-05-05 15:42:31 +0300

    -

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,6 +68,7 @@ const config: QuartzConfig = {
       }),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
+      Plugin.FrontMatter(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
